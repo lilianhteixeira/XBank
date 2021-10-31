87a1cc3758b6b228e63d1d5ec076bbbab206aa30
Tassila Bomfim <tassila.bomfim@xpi.com.br>
2021-10-31 14:19:08 -0300

    criando classe conta

---
+++ src/Domain/Core/Entities/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XBank.Domain.Core.Entities
+{
+    public class Account
+    {
+        public Guid IdClient { get; set; }
+        public decimal Balance { get; private set; }
+        private List<Moviment> Moviment { get; set; }
+
+        public List<string> GenerateExtract()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Transfer()
+        {
+            throw new NotImplementedException();
+        }
+        public void Withdraw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

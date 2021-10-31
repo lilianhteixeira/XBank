35cdb60dff629a42faf7a1b1770eade0a47f37b0
Vitor R <vitor_rc@outlook.com>
2021-10-31 15:19:27 -0300

    Configuração inicial

---
+++ src/Infra/Configs/EntityConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using XBank.Domain.Shared.Entities;
+
+namespace XBank.Domain.Infra.Configs
+{
+    public class EntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : Entity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder
+                .Property(x => x.Id)
+                .HasColumnName("ID");
+
+            builder
+                .Property(x => x.CreatedAt)
+                .HasColumnName("DT_CREATED");
+
+            builder
+                .Property(x => x.UpdatedAt)
+                .HasColumnName("DT_UPDATED");
+
+            builder
+                .Property(x => x.IsActive)
+                .HasColumnName("ST_ACTIVE");
+        }
+    }
+}

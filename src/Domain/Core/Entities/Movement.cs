d34cebdacaf221969d03029adc0feb0cd996c677
Simoni Accioly Vieira <simoni.accioly@xpi.com.br>
2021-10-31 13:47:57 -0300

    Inserindo classe Movement

---
+++ src/Domain/Core/Entities/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XBank.Domain.Core.Entities
+{
+    class Movement
+    {
+        public Guid IdAccount { get; set; }
+        public int MovementValue { get; set; }
+        public string CPF { get; set; }
+    }
+}

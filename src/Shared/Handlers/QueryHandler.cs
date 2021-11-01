fa55fd4e94f931251a62b54599d42740acce33cc
Vitor R <vitor_rc@outlook.com>
2021-11-01 10:08:01 -0300

    Adicionado as classes bases e interfaces dos handlers

---
+++ src/Shared/Handlers/QueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XBank.Domain.Shared.Entities;
+using XBank.Domain.Shared.Interfaces;
+using XBank.Domain.Shared.Requests;
+
+namespace XBank.Domain.Shared.Handlers
+{
+    public abstract class QueryHandler<TEntity, TRequest, TResponse>
+        where TEntity : Entity
+        where TRequest : Request
+    {
+        protected readonly IQueryRepository<TEntity> _repository;
+
+        public QueryHandler(IQueryRepository<TEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public abstract TResponse Handle(TRequest request);
+    }
+}
